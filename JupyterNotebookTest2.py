c107fd0edd4a429f0f1011bdf0c2a6fa738f9cb5
Ozi Handicraft <aaron.remski@gmail.com>
2020-11-02 22:44:09 -0600

    added jupytext convert

---
+++ JupyterNotebookTest2.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+x = np.arange(-3, 3, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
+# -
+
+
