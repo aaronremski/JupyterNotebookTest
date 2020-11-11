517aa45d156b4589ede094ebc748ec004fa91083
Ozi Handicraft <aaron.remski@gmail.com>
2020-11-10 21:23:50 -0600

    added pcolor graph

--- JupyterNotebookTest2.py
+++ JupyterNotebookTest2.py
@@ -1,10 +1,11 @@
 # ---
 # jupyter:
 #   jupytext:
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
+#       format_name: percent
+#       format_version: '1.3'
 #       jupytext_version: 1.6.0
 #   kernelspec:
 #     display_name: Python 3
@@ -12,15 +13,31 @@
 #     name: python3
 # ---
 
-# +
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
 # %matplotlib inline
 
+# %%
 x = np.arange(-3, 6, 0.1)
 y = np.sin(x)
 plt.plot(x, y)
 plt.show()
-# -
 
+# %%
+Z = np.random.rand(6, 10)
 
+fig, (ax0, ax1) = plt.subplots(2, 1)
+
+c = ax0.pcolor(Z)
+ax0.set_title('default: no edges')
+
+c = ax1.pcolor(Z, edgecolors='k', linewidths=4)
+ax1.set_title('thick edges')
+
+fig.tight_layout()
+plt.show()
+
+# %%
